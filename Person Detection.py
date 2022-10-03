ee792062405f90f10f4623ae712ba44894a0a214
Programmer1325 <krishith.vadivel@gmail.com>
2022-10-03 19:51:38 +0530

    Body Detection added

--- Person Detection.py
+++ Person Detection.py
@@ -1,2 +1,25 @@
-print("Hello world")
-print("Another Test")
+import cv2, time
+
+Face_Cascade = cv2.CascadeClassifier('Upper_Body_Set.xml')
+
+VidCapture = cv2.VideoCapture(0)
+
+while True:
+    time.sleep(5)
+    _, Frame = VidCapture.read()
+
+    GrayscaleImage = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+
+    Faces = Face_Cascade.detectMultiScale(GrayscaleImage, 1.01, 6)
+
+    if len(Faces) > 0:
+        print("In Seat")
+    else:
+        print("Not in Seat")
+
+
+    Keypress = cv2.waitKey(30) & 0xff
+    if Keypress == 27:
+        break
+
+VidCapture.release()
---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+This software detects whether a person is in front of a camera, and if there is no one present, does a specific set of functions
